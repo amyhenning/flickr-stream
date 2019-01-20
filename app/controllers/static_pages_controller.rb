97fb2dff15c8b4416d85d1061399d186891787b1
amyhenning <henning.amy@gmail.com>
2019-01-19 21:47:58 -0700

    Set up queries with usernames to return photos from the API, displaying photo count on home page for now

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
+	include StaticPagesHelper
 	def home
 		@user_id = params[:user_id]
+		if !@user_id.blank?
+			get_photos
+		end
 	end
 end
--- app/helpers/flickr_helper.rb
+++ app/helpers/flickr_helper.rb
@@ -1,10 +1,2 @@
 module FlickrHelper
-
 end
-
-
-# get_user_id(user_name) # use an inputted username to get the user's ID #
-
-# get_photos(user_id, limit) # use a user's ID # to get X number of photos from their feed
-
-# show_photos # show the photos from the user's stream
