76042deb67749b9abe4ce47d406d83dabbd3436f
amyhenning <henning.amy@gmail.com>
2019-01-20 20:48:24 -0700

    Added some styles and checked browser compatibility

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,6 +1,6 @@
 module StaticPagesHelper
 	def get_photos
-		@flickr = FlickRaw::Flickr.new
-		@user_photos = @flickr.photos.search(:user_id => params[:user_id], :per_page => 20)
+		flickr = FlickRaw::Flickr.new
+		@user_photos = flickr.photos.search(:user_id => params[:user_id], :per_page => 20)
 	end
 end
