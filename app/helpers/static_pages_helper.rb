01a289e7a0895b19b1e95c12c6c9ca4fb2ccf258
amyhenning <henning.amy@gmail.com>
2019-01-21 21:22:42 -0700

    Added basic json error message for failed searches

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,6 +1,10 @@
 module StaticPagesHelper
 	def get_photos
 		flickr = FlickRaw::Flickr.new
-		@user_photos = flickr.photos.search(:user_id => params[:user_id], :per_page => 20)
+		begin
+			@user_photos = flickr.photos.search(:user_id => params[:user_id], :per_page => 20)
+		rescue FlickRaw::FailedResponse => e
+			render json: "Search failed: #{e.msg}", status: :unprocessable_entity
+		end
 	end
 end
