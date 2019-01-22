7e7f606ace3840d09012cdbf772742aeef873df9
amyhenning <henning.amy@gmail.com>
2019-01-21 21:52:40 -0700

    Separated user photos and user errors into partial views, to be rendered based on API response

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -2,9 +2,10 @@ module StaticPagesHelper
 	def get_photos
 		flickr = FlickRaw::Flickr.new
 		begin
-			@user_photos = flickr.photos.search(:user_id => params[:user_id], :per_page => 20)
-		rescue FlickRaw::FailedResponse => e
-			render json: "Search failed: #{e.msg}", status: :unprocessable_entity
+			@user_photos = flickr.photos.search(:user_id => @user_id, :per_page => 20)
+			render partial: '/flickr/user_photos'
+		rescue FlickRaw::FailedResponse
+			render partial: '/flickr/user_error'
 		end
 	end
 end
